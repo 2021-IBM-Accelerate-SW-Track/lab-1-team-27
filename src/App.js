45d200ee12c75317675bc3e213100ef5a302eb12
Anushka <aacheria@uci.edu>
2021-06-15 12:04:40 -0700

    Basic text box and button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Header from "./component/header"
+//import Header from "./component/header"
 import './App.css';
+
+var lists = [];
+
+function addToList (props)
+{
+  lists.push(props.item);
+}
 
 function App() {
   return (
     <div className="App">
-    <Header/>
+      <h1>To Do List</h1>
+      <button>Add Item</button>
+      <input></input>
+      <ul>
+        <li>First Item</li>
+      </ul>
     </div>
   );
 }
