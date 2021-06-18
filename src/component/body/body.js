0a72bad46439d67ebbbc9eedb8192947e935a0c8
alay <artjj09@gmail.com>
2021-06-18 13:39:44 -0700

    friday

--- src/component/body/body.js
+++ src/component/body/body.js
@@ -14,9 +14,17 @@ const useStyles = makeStyles((theme) => ({
   
 }));
 
-export default function BasicTextFields(props) {
+export default 
+function BasicTextFields(props) {
   
   const[myList, setMyList] = useState([]); //trying out the map function
+
+  function handleDelete(item) {
+    const newList =  myList.filter(value => value !== item);
+    console.log(newList); 
+    setMyList(newList);
+  }
+
   const listItems = myList.map((item)=> 
   <li key={item.toString()}>{item}</li>
   );
@@ -33,30 +41,49 @@ export default function BasicTextFields(props) {
   const handleSubmit = (i) =>  {
    i.preventDefault()
     
-   const uid = function(){
-    return  Math.floor(Math.random() * 10000)
-}
+  // props.onSubmit({
+        
+    //id: Math.floor(Math.random() * 10000),//creates an id for each entry by creating a random number, but with a lower chance of there being similar ids
 
-   if(listItems ){ //I think this is where you can validate duplicate entries 
-        console.log(myList, uid(listItems))
+    //text:myList,
+    
+    //value: myList,
+   //})
+
+   //if(value){ //I think this is where you can validate duplicate entries 
+        //console.log(myList)
         
        
         setMyList(myList.concat([value]));
-   }
+   //}
+   
+   console.log(myList);
+
+   for(let i = 0; i < myList.length; i++){
+
+    if(myList[i] == value){
+      alert("one or more of your todos are the same!");
+      handleDelete(i);
+    } 
+    
+  }
 
  
-   function checkIfDuplicateExists(w){
-    return new Set(w).size !== w.length 
-}
+   //function checkIfDuplicateExists(w){
+   // return new Set(w).size !== w.length
+//}
 
-console.log(
-  checkIfDuplicateExists(myList)
+//console.log(
+  //checkIfDuplicateExists(myList)
 // true
-);
+//);
 
-if(checkIfDuplicateExists(myList)){
-  alert("Two or more of your todos are the same!");
-}
+//if(checkIfDuplicateExists(myList)){
+  //alert("Two or more of your todos are the same!");
+  //return
+//}
+
+
     
 
    //let SearchForDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
@@ -76,19 +103,26 @@ if(checkIfDuplicateExists(myList)){
       id="outlined-basic"
       label="Add To-do Item"
       variant="outlined"
+      data-testid="new-item-input" 
       onChange={(i) => setValue(i.target.value)}
       multiline
      rowsMax={2}/>
+
+     
 
       
       <Button
         type="submit"
         variant="outlined"
         size = "large"
+        data-testid="new-item-button" 
         >Add Item</Button>
 
-      <div style = {divStyle}>{listItems}</div>  
+        
+
+<div style = {divStyle}>{listItems}</div>  
     </form>
+   
   )
   }
 
